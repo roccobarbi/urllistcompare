7292eb5fe5797d74218f6ae413876019db1c8f57
Rocco Barbini <roccobarbi@gmail.com>
2017-05-28 14:19:34 +0200

    package-info updated

--- src/urllistcompare/package-info.java
+++ src/urllistcompare/package-info.java
@@ -1,8 +1,11 @@
 /**
- * 
+ * Classes and enumerations that enable easy comparisons of lists of URL
+ * tracked with different digital analytics tools (e.g. Webtrekk and Google Analytics)
+ * to see if some areas are missing on one of them.
  */
 /**
- * @author User
+ * @author Rocco Barbini
+ * @email roccobarbi@gmail.com
  *
  */
 package urllistcompare;
