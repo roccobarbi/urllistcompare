6716feb11b632190cb3b862d34f68063ec29a20a
Rocco Barbini <roccobarbi@gmail.com>
2017-06-03 22:07:38 +0200

    Various improvements on URLNorm

--- src/urllistcompare/URLNorm.java
+++ src/urllistcompare/URLNorm.java
@@ -69,24 +69,45 @@ public class URLNorm {
 	}
 	
 	/**
+	 * 
+	 * @param index the index of the format array that needs to be set
+	 * @param format the new format
+	 * @return true in case of success
+	 */
+	public boolean setFormat(int index, URLFormat format){
+		if(index < 0 || index > 1){
+			throw new IndexOutOfBoundsException("Tried to set format lower than 0 or greater than 1!");
+		}
+		this.format[index] = format;
+		return true;
+	}
+	
+	/**
 	 * Adds a URLElement instance to the relevant LinkeList and adds its page impressions count to the relevant array.
 	 * Checks that both the URLElement and the current URLNorm are properly set.
 	 * 
 	 * @param u	the URLElement that needs to be added
 	 * @throws InvalidURLNormException if at least one of the formats has not been set correctly or if the URL has the wrong format
+	 * @throws RuntimeException if the URLElement is in a format that is not included in the URLNorm instance
 	 */
 	public void add(URLElement u) {
 		if(format[0] == null || format[1] == null) throw new InvalidURLNormException("Tried to add an element" + 
 				"without defining both formats.");
 		try{
 			if(u.getFormat() == format[0]){
-				if(!elements[0].add(u)) System.err.println("Tried to add URLElement " + u.getUrl() + " twice!");
-				impressions[0] += u.getImpressions();
+				if(!elements[0].add(u)){
+					System.err.println("Tried to add URLElement " + u.getUrl() + " twice!");
+				} else {
+					impressions[0] += u.getImpressions();
+				}
 			} else if (u.getFormat() == format[1]){
-				if(!elements[1].add(u)) System.err.println("Tried to add URLElement " + u.getUrl() + " twice!");
-				impressions[1] += u.getImpressions();
+				if(!elements[1].add(u)){
+					System.err.println("Tried to add URLElement " + u.getUrl() + " twice!");
+				} else {
+					impressions[1] += u.getImpressions();
+				}
 			} else {
-				System.err.println("Invalid format: tried to add to a URLNorm instance a URLElement instance with the wrong URLFormat.");
+				throw new RuntimeException("Tried to add a URLElement in the wrong format to a URLNorm instance!");
 			}
 		} catch (InvalidUrlException e) {
 			System.err.println("InvalidUrlException: tried to add an empty URLElement instance to a URLNorm instance.");
@@ -101,28 +122,38 @@ public class URLNorm {
 	 * @throws InvalidURLNormException	if at least one of the formats has not been set correctly or the wrong URLFormat is passed as an argument
 	 */
 	public boolean isMissing(URLFormat f) throws InvalidURLNormException {
-		boolean output = false;
 		if(format[0] == null || format[1] == null) throw new InvalidURLNormException("Tried to check if a format is missing" + 
 				"without defining both formats.");
 		if(format[0] != f && format[1] != f) throw new InvalidURLNormException("Tried to check if a format is missing" + 
 				"with a format that is not included in this URLNorm instance.");
-		if((f == format[0] && impressions[0] == 0) || (f == format[1] && impressions[1] == 0)){
-			output = true;
-		}
-		return output;
+		return ((f == format[0] && impressions[0] == 0) || (f == format[1] && impressions[1] == 0));
 	}
 	
 	/**
-	 * Checks the absolute difference between the page impressions of the specified format and those of the other format.
-	 * If there are formats A and B, andad format A is passed as argument, the result is A - B.
+	 * Checks the absolute difference between the page impressions of format 0 and 1.
+	 * The result is [0] - [1]
 	 * 
 	 * @param f the format to check
 	 * @return the difference of f relative to the other format
 	 * @throws InvalidURLNormException	if at least one of the formats has not been set correctly or the wrong URLFormat is passed as an argument
 	 */
-	public int getDifference(URLFormat f) throws InvalidURLNormException {
-		int output = 0;
+	public int getDifference(){
 		if(format[0] == null || format[1] == null) throw new InvalidURLNormException("Tried to check if a format is missing" + 
+				"without defining both formats.");
+		return impressions[0] - impressions[1];
+	}
+	
+	/**
+	 * Checks the absolute difference between the page impressions of the specified format and those of the other format.
+	 * If there are formats A and B, and format A is passed as argument, the result is A - B.
+	 * 
+	 * @param f the format to check
+	 * @return the difference of f relative to the other format
+	 * @throws InvalidURLNormException	if at least one of the formats has not been set correctly or the wrong URLFormat is passed as an argument
+	 */
+	public int getDifference(URLFormat f){
+		int output = 0;
+		if(format[0] == null || format[1] == null) throw new InvalidURLNormException("Tried to check the difference between the formats" + 
 				"without defining both formats.");
 		if(format[0] != f && format[1] != f) throw new InvalidURLNormException("Tried to check if a format is missing" + 
 				"with a format that is not included in this URLNorm instance.");
@@ -157,11 +188,10 @@ public class URLNorm {
 		return output;
 	}
 	
-	// TODO: needs improvement
 	@Override
 	public int hashCode(){
 		if (url == null){
-			return -1;
+			throw new InvalidURLNormException("Tried to get the hashCode of a URLNorm instance without a url value!");
 		}
 		return url.hashCode();
 	}
