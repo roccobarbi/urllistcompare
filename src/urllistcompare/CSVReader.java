b368f267a01ecba95f119751849401c5d5c24bf2
Rocco Barbini <roccobarbi@gmail.com>
2017-06-04 16:37:34 +0200

    csv reader and writer outlined

---
+++ src/urllistcompare/CSVReader.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package urllistcompare;
+
+/**
+ * @author Rocco Barbini (roccobarbi@gmail.com)
+ * 
+ * This class is tasked with reading the contents of a CSV file into a URLList.
+ * It can receive the format, otherwise it will prompt the user to receive it.
+ * It can be set with the presence of headers, thousand and decimal separators, value separator,
+ * with the column numbers for the page impressions and the url. Otherwise, it prompts the user
+ * for those values.
+ * In case this class is used with a GUI, those values should be set in the constructor or with accessor
+ * functions before the class start reading the file. 
+ *
+ */
+public class CSVReader {
+
+	/**
+	 * 
+	 */
+	public CSVReader() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/urllistcompare/CSVWriter.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package urllistcompare;
+
+/**
+ * @author Rocco Barbini (roccobarbi@gmail.com)
+ * 
+ * This class is tasked with writing the contents of either an iterable set of URLElements or an array of URLElements
+ * to a CSV file.
+ * It can be set with the names of the headers, the thousand and decimal separators, the value separator.
+ * Otherwise, it prompts the user for those values.
+ * In case this class is used with a GUI, those values should be set in the constructor or with accessor
+ * functions before the class start writing the file. 
+ *
+ */
+public class CSVWriter {
+
+	/**
+	 * 
+	 */
+	public CSVWriter() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
