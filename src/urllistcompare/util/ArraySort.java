f9e20b86b893619397c3d247eef7f9e52602bcb3
Rocco Barbini <roccobarbi@gmail.com>
2017-06-04 15:05:32 +0200

    ArraySort added

---
+++ src/urllistcompare/util/ArraySort.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package urllistcompare.util;
+
+/**
+ * @author Rocco Barbini (roccobarbi@gmail.com)
+ *
+ */
+public final class ArraySort {
+
+	public static <T extends Comparable<? super T>> T[] insertionSortAsc(T[] a){
+		int i, k;
+		T temp;
+		if(a.length > 1){
+			for(i = 1; i < a.length; i++){
+				k = i;
+				while(k > 0 && a[k].compareTo(a[k - 1]) <= 0){
+					temp = a[k];
+					a[k] = a[k-1];
+					a[k-1] = temp;
+				}
+			}
+		}
+		return a;
+	}
+	
+	public static <T extends Comparable<? super T>> T[] insertionSortDesc(T[] a){
+		int i, k;
+		T temp;
+		if(a.length > 1){
+			for(i = 1; i < a.length; i++){
+				k = i;
+				while(k > 0 && a[k].compareTo(a[k - 1]) >= 0){
+					temp = a[k];
+					a[k] = a[k-1];
+					a[k-1] = temp;
+				}
+			}
+		}
+		return a;
+	}
+
+}
