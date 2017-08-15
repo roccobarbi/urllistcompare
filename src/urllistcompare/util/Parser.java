bc9d3cb7928a9180865bfebbd1b18eb9dc4219b6
Rocco Barbini <roccobarbi@gmail.com>
2017-08-15 08:34:15 +0200

    Parser class added

---
+++ src/urllistcompare/util/Parser.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package urllistcompare.util;
+
+/**
+ * @author Rocco Barbini (roccobarbi@gmail.com)
+ * 
+ * Utility class that contains methods to parse values into the program.
+ *
+ */
+public final class Parser {
+
+	/**
+	 * 
+	 */
+	public Parser() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
