73ac5df8b5ad86035219ebb4aecbc6a02cf09139
Rocco Barbini <roccobarbi@gmail.com>
2017-10-24 21:45:54 +0200

    superfluous linked list implementation removed

--- src/urllistcompare/util/ArraySort.java
+++ src/urllistcompare/util/ArraySort.java
@@ -9,38 +9,45 @@ package urllistcompare.util;
  */
 public final class ArraySort {
 
-	public static <T extends Comparable<? super T>> T[] insertionSortAsc(T[] a){
+	public static <T extends Comparable<? super T>>
+		T[]
+		insertionSortAsc(T[] a) {
 		int i, k;
 		T temp;
-		if(a.length > 1){
-			for(i = 1; i < a.length; i++){
+		if (a.length > 1) {
+			for (i = 1; i < a.length; i++) {
 				k = i;
-				while(k > 0 && a[k].compareTo(a[k - 1]) < 0){
+				while (k > 0 && a[k].compareTo(a[k - 1]) < 0) {
 					temp = a[k];
-					a[k] = a[k-1];
-					a[k-1] = temp;
+					a[k] = a[k - 1];
+					a[k - 1] = temp;
 					k--;
 				}
 			}
 		}
 		return a;
 	}
-	
-	public static <T extends Comparable<? super T>> T[] insertionSortDesc(T[] a){
+
+	public static <T extends Comparable<? super T>>
+		T[]
+		insertionSortDesc(T[] a) {
 		int i, k;
 		T temp;
-		if(a.length > 1){
-			for(i = 1; i < a.length; i++){
+		if (a.length > 1) {
+			for (i = 1; i < a.length; i++) {
 				k = i;
-				while(k > 0 && a[k].compareTo(a[k - 1]) > 0){
+				while (k > 0 && a[k].compareTo(a[k - 1]) > 0) {
 					temp = a[k];
-					a[k] = a[k-1];
-					a[k-1] = temp;
+					a[k] = a[k - 1];
+					a[k - 1] = temp;
 					k--;
 				}
 			}
 		}
 		return a;
 	}
+
+	// TODO: implement a quicksort and/or a mergesort, plus a generic sort metod
+	// that applies the best algorithm depending on the size of the input.
 
 }
