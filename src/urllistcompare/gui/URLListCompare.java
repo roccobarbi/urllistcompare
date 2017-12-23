55a01c9a9a381eb663db86ad60543ce4d40bf28a
Rocco Barbini <roccobarbi@gmail.com>
2017-12-23 23:41:55 +0100

    some more preparatory work done

--- src/urllistcompare/gui/URLListCompare.java
+++ src/urllistcompare/gui/URLListCompare.java
@@ -23,34 +23,104 @@ public class URLListCompare extends JFrame {
 		Container cp = getContentPane();
 
 		// Set the layout
-		cp.setLayout(new BorderLayout(10,4));
-		
-		// main phone buttons
+		cp.setLayout(new BorderLayout());
+
+		// Top Panel
+		JPanel topPanel = new JPanel();
+		GroupLayout topLayout = new GroupLayout(topPanel);
+		topLayout.setAutoCreateGaps(true);
+		topLayout.setAutoCreateContainerGaps(true);
+
+		// Program Status
+		JLabel programStatus = new JLabel("Program Status");
+		JTextField statusIndicator = new JTextField("INACTIVE", 10);
+		statusIndicator.setForeground(Color.red);
+		statusIndicator.setBackground(Color.orange);
+		statusIndicator.setEditable(false);
+
+		// Extension
+		JLabel extension = new JLabel("Extension");
+		JTextField extensionIndicator = new JTextField("ON", 4);
+		extensionIndicator.setForeground(new Color(85, 107, 47));
+		extensionIndicator.setBackground(new Color(0, 255, 0));
+		extensionIndicator.setEditable(false);
+		JButton extensionToggle = new JButton("Extension Toggle");
+		extensionToggle
+				.setToolTipText("Toggle file extensions in the URL List.");
+		extensionToggle.setEnabled(true);
+		extensionToggle.setPreferredSize(new Dimension(100, 20));
+
+		// Load Files
+		JButton loadFiles = new JButton("Load Files");
+		loadFiles.setToolTipText(
+				"Open the screen that manages the operations needed to load"
+						+ " the input csv files and create a URL List.");
+		loadFiles.setEnabled(true); // Default when opening the program
+		loadFiles.setPreferredSize(new Dimension(100, 20));
+
+		// Load Binary
+		JButton loadBinary = new JButton("Load Binary");
+		loadBinary.setToolTipText(
+				"Open the screen that manages the operations needed to load"
+						+ " the input binary file from a previous URL List.");
+		loadBinary.setEnabled(true); // Default when opening the program
+		loadBinary.setPreferredSize(new Dimension(100, 20));
+
+		// Add elements to top panel layout
+		topLayout.setHorizontalGroup(topLayout.createSequentialGroup()
+				.addGroup(topLayout
+						.createParallelGroup(GroupLayout.Alignment.LEADING)
+						.addGroup(topLayout.createSequentialGroup()
+								.addComponent(programStatus)
+								.addComponent(statusIndicator))
+						.addGroup(topLayout.createSequentialGroup()
+								.addComponent(extension)
+								.addComponent(extensionIndicator)
+								.addComponent(extensionToggle))
+						.addGroup(topLayout
+								.createParallelGroup(
+										GroupLayout.Alignment.LEADING)
+								.addComponent(loadFiles)
+								.addComponent(loadBinary))));
+		topLayout.setVerticalGroup(topLayout.createSequentialGroup().addGroup(
+				topLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+				.addComponent(programStatus)
+				.addComponent(statusIndicator)
+				.addComponent(loadFiles))
+				.addGroup(topLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+						.addComponent(extension)
+						.addComponent(extensionIndicator)
+						.addComponent(extensionToggle)
+						.addComponent(loadBinary)));
+
+		// Top Panel Add
+		cp.add(topPanel, BorderLayout.NORTH);
+
 		JPanel centerPanel = new JPanel(new GridLayout(4, 3));
 		cp.add(centerPanel, BorderLayout.CENTER);
 		cp.add(new JLabel("East"), BorderLayout.EAST);
 		cp.add(new JLabel("West"), BorderLayout.WEST);
-		
+
 		JTextField hasHeaderA = new JTextField(7);
 		hasHeaderA.setText("Header");
 		hasHeaderA.setForeground(Color.red);
 		hasHeaderA.setBackground(Color.orange);
-		hasHeaderA.addMouseListener(new MouseAdapter(){
-            @Override
-            public void mouseClicked(MouseEvent e){
-            	JTextField source = (JTextField) e.getSource();
-                if(source.getText().equals("Header")) {
-                	source.setText("Toggled");
-                	source.setForeground(Color.green);
-                	source.setBackground(Color.white);
-                }
-                else {
-                	source.setText("Header");
-                	source.setForeground(Color.red);
-                	source.setBackground(Color.orange);
-                }
-            }
-        });
+		hasHeaderA.addMouseListener(new MouseAdapter() {
+			@Override
+			public void
+				mouseClicked(MouseEvent e) {
+				JTextField source = (JTextField) e.getSource();
+				if (source.getText().equals("Header")) {
+					source.setText("Toggled");
+					source.setForeground(Color.green);
+					source.setBackground(Color.white);
+				} else {
+					source.setText("Header");
+					source.setForeground(Color.red);
+					source.setBackground(Color.orange);
+				}
+			}
+		});
 		hasHeaderA.setEditable(false);
 		centerPanel.add(hasHeaderA);
 
