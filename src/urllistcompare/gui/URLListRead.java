9ec9a4f4b9922230861f6fd92a075d7e9966d66b
Rocco Barbini <roccobarbi@gmail.com>
2017-12-23 23:42:15 +0100

    URLListRead class added

---
+++ src/urllistcompare/gui/URLListRead.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package urllistcompare.gui;
+
+/**
+ * This class manages the settings and operations needed to read the input csv
+ * files. The purpose is the same as that of the CSVReader class for the CLI
+ * version of this software.
+ * 
+ * @author rocco barbini (roccobarbi@gmail.com)
+ *
+ */
+public class URLListRead {
+
+}
