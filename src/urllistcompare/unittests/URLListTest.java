3d0719d2ec4bd750ada469b2ee9493d58f5bba82
Rocco Barbini <roccobarbi@gmail.com>
2018-08-15 19:48:01 +0200

    comparing two lists of the same format partially tested

--- src/urllistcompare/unittests/URLListTest.java
+++ src/urllistcompare/unittests/URLListTest.java
@@ -64,6 +64,16 @@ public class URLListTest {
 		assertFalse("Did not return false after not setting the format!", list001.setFormat(1, URLFormat.FULLURL));
 		assertTrue("Wrong first format, it should not have changed!", list001.getFormat(0) == URLFormat.WTKDEF);
 	}
+	
+	@Test
+	public void testSetSameFormat() {
+		try{
+			@SuppressWarnings("unused")
+			URLList list001 = new URLList(URLFormat.WTKDEF, URLFormat.WTKDEF, false);
+		} catch (Exception e) {
+			fail("Raised an exception when trying to set up a URLList with two elements of the same format");
+		}
+	}
 
 	@Test
 	public void testIsActive() {
@@ -98,6 +108,23 @@ public class URLListTest {
 		} catch (RuntimeException e) {
 			assertTrue("Wrong error message!", e.getMessage().equals("Tried to add a URLElement in the wrong format to a URLList instance!"));
 		}
+	}
+	
+	@Test
+	public void testAddSameFormat() {
+		URLList list001 = new URLList();
+		URLElement element001 = new URLElement("www_domain_com.path1.path2.file_ext", URLFormat.WTKDEF, 1200);
+		URLElement element002 = new URLElement("www_domain_com.path1.path2.file_ext", URLFormat.WTKDEF, 200);
+		try{
+			list001.add(element001, 0);
+			fail("Did not raise an exception while the list was not active!");
+		} catch (InvalidURLListException e) {
+			// Passed
+		}
+		list001.setFormat(0, URLFormat.WTKDEF);
+		list001.setFormat(1, URLFormat.WTKDEF);
+		assertTrue("Element not added when it should have been!", list001.add(element001, 0));
+		assertTrue("Element not added when it should have been!", list001.add(element002, 1));
 	}
 
 	@Test
@@ -142,6 +169,29 @@ public class URLListTest {
 		assertTrue("Wrong first element in output001", output001[0].equals(element002));
 		assertTrue("Wrong second element in output001", output001[1].equals(element004));
 		assertTrue("Wrong length for output002: " + output002.length, output002.length == 0);
+	}
+	
+	@Test
+	public void testGetMissingElementsIntSameFormat() {
+		URLList list001 = new URLList();
+		URLElement output001[];
+		URLElement output002[];
+		list001.setFormat(0, URLFormat.URLNORM);
+		list001.setFormat(1, URLFormat.URLNORM);
+		URLElement element001 = new URLElement("www.domain.com/path1/path2/file2.ext", URLFormat.URLNORM, 1200);
+		URLElement element002 = new URLElement("www.domain.com/path1/path2/file.ext", URLFormat.URLNORM, 1200);
+		URLElement element003 = new URLElement("www.domain.com/path1/path2/file2.ext", URLFormat.URLNORM, 200);
+		list001.add(element001, 0);
+		list001.add(element002, 0);
+		list001.add(element003, 1);
+		output001 = list001.getMissingElements(0);
+		output002 = list001.getMissingElements(1);
+		for(URLElement e : output001){
+			System.out.println(e);
+		}
+		assertTrue("Wrong length for output001: " + output001.length, output001.length == 0);
+		assertTrue("Wrong length for output002: " + output002.length, output002.length == 1);
+		assertTrue("Wrong first element in output001", output002[0].equals(element002));
 	}
 
 	@Test
