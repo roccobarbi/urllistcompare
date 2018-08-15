f24ab9593ffee7f0dbff0a262a85322868d6be07
Rocco Barbini <roccobarbi@gmail.com>
2018-08-15 19:06:37 +0200

    adapted to the new builder pattern

--- src/urllistcompare/unittests/CSVReaderTest.java
+++ src/urllistcompare/unittests/CSVReaderTest.java
@@ -2,6 +2,8 @@ package urllistcompare.unittests;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+
 import org.junit.Test;
 
 import urllistcompare.CSVReader;
@@ -13,14 +15,34 @@ public class CSVReaderTest {
 	@Test
 	public void testRead() {
 		URLList list = new URLList(URLFormat.URLNORM, URLFormat.GOOG, false);
-		CSVReader reader = new CSVReader(true, 0, 1, ';', ',', true, '.', "test001.txt", URLFormat.URLNORM);
+		CSVReader reader = new CSVReader
+				.CSVReaderBuilder(new File("test001.txt"))
+				.headers(true)
+				.urlI(0)
+				.impI(1)
+				.vSep(';')
+				.dSep('.')
+				.isTSep(true)
+				.tSep(',')
+				.format(URLFormat.URLNORM)
+				.build();
 		reader.setDestination(list);
 		try{
 			reader.read();
 		} catch (Exception e){
 			fail("Exception where none should have been thrown: " + e.getMessage());
 		}
-		reader = new CSVReader(true, 0, 1, ';', ',', true, '.', "test002.txt", URLFormat.URLNORM);
+		reader = new CSVReader
+				.CSVReaderBuilder(new File("test002.txt"))
+				.headers(true)
+				.urlI(0)
+				.impI(1)
+				.vSep(';')
+				.dSep(',')
+				.isTSep(true)
+				.tSep('.')
+				.format(URLFormat.URLNORM)
+				.build();
 		list = new URLList(URLFormat.URLNORM, URLFormat.GOOG, false);
 		reader.setDestination(list);
 		try{
@@ -28,7 +50,17 @@ public class CSVReaderTest {
 		} catch (Exception e){
 			fail("Exception where none should have been thrown: " + e.getMessage());
 		}
-		reader = new CSVReader(true, 0, 1, ';', ',', true, '.', "test003.txt", URLFormat.URLNORM);
+		reader = new CSVReader
+				.CSVReaderBuilder(new File("test003.txt"))
+				.headers(true)
+				.urlI(0)
+				.impI(1)
+				.vSep(';')
+				.dSep(',')
+				.isTSep(true)
+				.tSep('.')
+				.format(URLFormat.URLNORM)
+				.build();
 		list = new URLList(URLFormat.URLNORM, URLFormat.GOOG, false);
 		reader.setDestination(list);
 		try{
@@ -37,7 +69,17 @@ public class CSVReaderTest {
 		} catch (Exception e){
 			System.out.println(e.getMessage());
 		}
-		reader = new CSVReader(true, 0, 1, ';', ',', true, '.', "test004.txt", URLFormat.URLNORM);
+		reader = new CSVReader
+				.CSVReaderBuilder(new File("test004.txt"))
+				.headers(true)
+				.urlI(0)
+				.impI(1)
+				.vSep(';')
+				.dSep(',')
+				.isTSep(true)
+				.tSep('.')
+				.format(URLFormat.URLNORM)
+				.build();
 		list = new URLList(URLFormat.URLNORM, URLFormat.GOOG, false);
 		reader.setDestination(list);
 		try{
@@ -46,7 +88,17 @@ public class CSVReaderTest {
 		} catch (Exception e){
 			System.out.println(e.getMessage());
 		}
-		reader = new CSVReader(true, 0, 1, ';', ',', true, '.', "test005.txt", URLFormat.URLNORM);
+		reader = new CSVReader
+				.CSVReaderBuilder(new File("test005.txt"))
+				.headers(true)
+				.urlI(0)
+				.impI(1)
+				.vSep(';')
+				.dSep(',')
+				.isTSep(true)
+				.tSep('.')
+				.format(URLFormat.URLNORM)
+				.build();
 		list = new URLList(URLFormat.URLNORM, URLFormat.GOOG, false);
 		reader.setDestination(list);
 		try{
@@ -54,7 +106,17 @@ public class CSVReaderTest {
 		} catch (Exception e){
 			fail("Exception where none should have been thrown: " + e.getMessage());
 		}
-		reader = new CSVReader(true, 0, 1, ';', ',', true, '.', "test006.txt", URLFormat.URLNORM);
+		reader = new CSVReader
+				.CSVReaderBuilder(new File("test006.txt"))
+				.headers(true)
+				.urlI(0)
+				.impI(1)
+				.vSep(';')
+				.dSep(',')
+				.isTSep(true)
+				.tSep('.')
+				.format(URLFormat.URLNORM)
+				.build();
 		list = new URLList(URLFormat.URLNORM, URLFormat.GOOG, false);
 		reader.setDestination(list);
 		try{
@@ -63,7 +125,17 @@ public class CSVReaderTest {
 		} catch (Exception e){
 			System.out.println(e.getMessage());
 		}
-		reader = new CSVReader(true, 0, 1, ';', ',', true, '.', "test007.txt", URLFormat.URLNORM);
+		reader = new CSVReader
+				.CSVReaderBuilder(new File("test007.txt"))
+				.headers(true)
+				.urlI(0)
+				.impI(1)
+				.vSep(';')
+				.dSep(',')
+				.isTSep(true)
+				.tSep('.')
+				.format(URLFormat.URLNORM)
+				.build();
 		list = new URLList(URLFormat.URLNORM, URLFormat.GOOG, false);
 		reader.setDestination(list);
 		try{
