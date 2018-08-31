4839019d16d3d286e02fc39ae1837365bbde082a
Rocco Barbini <roccobarbi@gmail.com>
2018-08-31 10:00:41 +0200

    MainTestSuite added

---
+++ src/urllistcompare/unittests/MainTestSuite.java
@@ -0,0 +1,19 @@
+package urllistcompare.unittests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+	ArraySortTest.class,
+	URLFormatTest.class,
+	URLElementTest.class,
+	URLNormTest.class,
+	URLListTest.class,
+	ParserTest.class
+})
+
+public class MainTestSuite {
+
+}
